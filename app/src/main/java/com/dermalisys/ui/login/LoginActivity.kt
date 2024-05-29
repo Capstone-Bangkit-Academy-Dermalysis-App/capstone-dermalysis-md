8cc79fd8f2efcce5114aebac2a0a923f9e6dfed9
Wisnu <136870014+Wisnuuh@users.noreply.github.com>
2024-05-29 19:48:55 +0700

    add login activity

---
+++ app/src/main/java/com/dermalisys/ui/login/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.dermalisys.ui.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.dermalisys.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
