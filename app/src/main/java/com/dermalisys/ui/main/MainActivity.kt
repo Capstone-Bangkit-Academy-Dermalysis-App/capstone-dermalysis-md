b00b931f1dd6d36181305c2b6ba1ac3ead22b06a
Wisnu <136870014+Wisnuuh@users.noreply.github.com>
2024-05-29 19:41:50 +0700

    organize package: -adding ui package -adding data package -adding main package -moving MainActivity to main package -adding login package -adding register package

--- app/src/main/java/com/dermalisys/ui/main/MainActivity.kt
+++ app/src/main/java/com/dermalisys/ui/main/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.dermalisys
+package com.dermalisys.ui.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.dermalisys.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
