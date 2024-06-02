77b8f83274f1b5e996d67fdf3d6d2c088939fe76
Wisnu <136870014+Wisnuuh@users.noreply.github.com.>
2024-06-02 14:22:38 +0700

    Update Login
    creating Login UI

--- app/src/main/java/com/dermalisys/ui/login/LoginActivity.kt
+++ app/src/main/java/com/dermalisys/ui/login/LoginActivity.kt
@@ -2,11 +2,40 @@ package com.dermalisys.ui.login
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
+import androidx.credentials.CredentialManager
+import androidx.credentials.GetCredentialRequest
 import com.dermalisys.R
+import com.dermalisys.databinding.ActivityLoginBinding
+import com.google.android.libraries.identity.googleid.GetGoogleIdOption
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
+        )
+
+        binding.googleLogin.setOnClickListener {
+
+        }
+    }
+
+    private fun signIn() {
+        val credentialManager = CredentialManager.create(this) //import from androidx.CredentialManager
+        val googleIdOption = GetGoogleIdOption.Builder()
+            .setFilterByAuthorizedAccounts(false)
+            .setServerClientId(getString(R.string.your_web_client_id))
+            .build()
+        val request = GetCredentialRequest.Builder() //import from androidx.CredentialManager
+            .addCredentialOption(googleIdOption)
+            .build()
     }
 }
--- app/src/main/java/com/dermalisys/ui/main/MainActivity.kt
+++ app/src/main/java/com/dermalisys/ui/main/MainActivity.kt
@@ -2,11 +2,17 @@ package com.dermalisys.ui.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
 import com.dermalisys.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
+        )
     }
 }
